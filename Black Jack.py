5f0d2177e139a034ea8ebaaee01c1cceeab2cabf
mesallc <113670261+mesallc@users.noreply.github.com>
2022-09-16 18:45:58 +0700

    Update Black Jack.py

--- Black Jack.py
+++ Black Jack.py
@@ -1,89 +1,89 @@
 import random
 
-KOLODA = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11] * 4 #колода карт
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11] * 4 #Колода карт
 
-random.shuffle(KOLODA) #Перемешиваем колоду
+random.shuffle(cards) #Перемешиваем колоду
 
-MAIN_KOLODA = 0 #Изначальный баланс игрока
-KRUPIE_KOLODA = 0 #Изначальный баланс крупье
+main_pack = 0 #Изначальный баланс игрока
+croupier_pack = 0 #Изначальный баланс крупье
 
-def GET_CARD_MAIN():
-    global MAIN_KOLODA #Обращение к глобальной переменной MAIN_KOLODA (Баланс игрока)
-    print('Выпала карта - ' + str(KOLODA[-1])) #Выводим сообщение о том, какая выпала карта
-    MAIN_KOLODA += KOLODA.pop() #Добавляем последнюю карту с колоды к балансу игрока, после удаляем её
+def get_cards_main():
+    global main_pack #Обращение к глобальной переменной main_pack (Баланс игрока)
+    print('Выпала карта - ' + str(cards[-1])) #Выводим сообщение о том, какая выпала карта
+    main_pack += cards.pop() #Добавляем последнюю карту с колоды к балансу игрока, после удаляем её
     return
 
-def GET_CARD_KRUPIE():
-    global KRUPIE_KOLODA #Обращение к глобальной переменной KRUPIE_KOLODA (Баланс крупье)
+def get_cards_croupier():
+    global croupier_pack #Обращение к глобальной переменной croupier_pack (Баланс крупье)
     #Добавляем немного человечности боту
-    if KRUPIE_KOLODA < 21 and KRUPIE_KOLODA < 20 and KRUPIE_KOLODA < 19 and KRUPIE_KOLODA < 18 and KRUPIE_KOLODA < 17:
-        KRUPIE_KOLODA += KOLODA.pop() #Добавляем последнюю карту с колоды к балансу бота, после удаляем её
+    if croupier_pack < 21 and croupier_pack < 20 and croupier_pack < 19 and croupier_pack < 18 and croupier_pack < 17:
+        croupier_pack += cards.pop() #Добавляем последнюю карту с колоды к балансу бота, после удаляем её
     return 
 
-def Win(): #Сообщение о выйгрыше, выводим сообщение с балансами обоих игроков
+def Win(): #Сообщение о выигрыше, выводим сообщение с балансами обоих игроков
     print('\n\n\n\n\n\n\n\n\n\n\n')
-    print('----[Вы выйграли]----')
-    print('Ваш баланс: ' + str(MAIN_KOLODA))
-    print('Баланс крупье: ' + str(KRUPIE_KOLODA))
+    print('----[Вы выиграли]----')
+    print('Ваш баланс: ' + str(main_pack)) #Баланс игрока
+    print('Баланс крупье: ' + str(croupier_pack)) #Баланс крупье
     print('---------------------\n')
     return
 
-def Lose(): #Сообщение о пройгрыше, выводим сообщение с балансами обоих игроков
+def Lose(): #Сообщение о проигрыше, выводим сообщение с балансами обоих игроков
     print('\n\n\n\n\n\n\n\n\n\n\n')
     print('----[Вы проиграли]----')
-    print('Ваш баланс: ' + str(MAIN_KOLODA))
-    print('Баланс крупье: ' + str(KRUPIE_KOLODA))
+    print('Ваш баланс: ' + str(main_pack)) #Баланс игрока
+    print('Баланс крупье: ' + str(croupier_pack)) #Баланс крупье
     print('----------------------\n')
     return
 
 def Nothing(): #Сообщение о ничьей, выводим сообщение с балансами обоих игроков
     print('\n\n\n\n\n\n\n\n\n\n\n')
     print('----[Ничья]----')
-    print('Ваш баланс: ' + str(MAIN_KOLODA))
-    print('Баланс крупье: ' + str(KRUPIE_KOLODA))
+    print('Ваш баланс: ' + str(main_pack)) #Баланс игрока
+    print('Баланс крупье: ' + str(croupier_pack)) #Баланс крупье
     print('---------------\n')
     return
 
-def GET_WINNER(): #Узнаём победителя
-    global KRUPIE_KOLODA #Обращение к глобальной переменной KRUPIE_KOLODA (Баланс крупье)
-    global MAIN_KOLODA #Обращение к глобальной переменной MAIN_KOLODA (Баланс игрока)
+def get_winner(): #Узнаём победителя
+    global croupier_pack #Обращение к глобальной переменной KRUPIE_KOLODA (Баланс крупье)
+    global main_pack #Обращение к глобальной переменной MAIN_KOLODA (Баланс игрока)
     
-    if MAIN_KOLODA <= 21 and KRUPIE_KOLODA > 21:
+    if main_pack <= 21 and croupier_pack > 21:
         Win() #Вывод сообщения о победе
-    if MAIN_KOLODA <= 21 and KRUPIE_KOLODA < 21 and MAIN_KOLODA > KRUPIE_KOLODA:
+    if main_pack <= 21 and croupier_pack < 21 and main_pack > croupier_pack:
         Win() #Вывод сообщения о победе
-    if MAIN_KOLODA > 21 and KRUPIE_KOLODA <= 21:
+    if main_pack > 21 and croupier_pack <= 21:
         Lose() #Вывод сообщения о пройгрыше
-    if MAIN_KOLODA < 21 and KRUPIE_KOLODA <= 21 and MAIN_KOLODA < KRUPIE_KOLODA:
+    if main_pack < 21 and croupier_pack <= 21 and main_pack < croupier_pack:
         Lose() #Вывод сообщения о пройгрыше
-    if MAIN_KOLODA > 21 and KRUPIE_KOLODA > 21:
+    if main_pack > 21 and croupier_pack > 21:
         Lose() #Вывод сообщения о пройгрыше
-    if MAIN_KOLODA == KRUPIE_KOLODA:
+    if main_pack == croupier_pack:
         Nothing() #Вывод сообщения о ничьей
     return
 
 while True:
     try:
-        if MAIN_KOLODA < 21:
-            print('Баланс: ', MAIN_KOLODA) #Выводим баланс игрока
-            SELECTION = input('Взять карту? [Y/N]: ') #Запрашиваем разрешение о выдаче карты
-            print('\n\n\n\n\n\n\n\n\n\n\n')
-            if SELECTION == 'Y' or SELECTION == 'y' or SELECTION == 'н' or SELECTION == 'Н':
+        if main_pack < 21:
+            print('Баланс: ', main_pack) #Выводим баланс игрока
+            choice = input('Взять карту? [Y/N]: ') #Запрашиваем разрешение о выдаче карты
+            print('\n\n\n\n\n\n\n\n\n\n\n') 
+            if choice == 'Y' or choice == 'y' or choice == 'н' or choice == 'Н':
                 
                 #При согласии, отдаем разные карты игроку и крупье
-                GET_CARD_KRUPIE() 
-                GET_CARD_MAIN() 
+                get_cards_croupier() #Крупье берёт карту
+                get_cards_main() #Игрок берёт карту
                 continue
 
-            if SELECTION == 'N' or SELECTION == 'n' or SELECTION == 'т' or SELECTION == 'Т': #Отказ от выдачи карты
-                GET_CARD_KRUPIE() #Крупье берет карту
-                GET_WINNER() #Выявляем победителя
+            if choice == 'N' or choice == 'n' or choice == 'т' or choice == 'Т': #Отказ от выдачи карты
+                get_cards_croupier() #Крупье берёт карту
+                get_winner() #Выявляем победителя
                 break
             else:
                 print('[Ошибка] Введите нужный символ') #Ошибка, если не введен нужный символ
                 continue
         else:
-            GET_WINNER()
+            get_winner() #Выявляем победителя
             break
     except ValueError:
         print('[Ошибка] Введите нужный символ') #Ошибка, если не введен нужный символ, завершаем работу
