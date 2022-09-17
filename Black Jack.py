d241c4ca456516621073772ed129751ec7a4d59d
mesallc <113670261+mesallc@users.noreply.github.com>
2022-09-18 00:06:04 +0700

    Update Black Jack.py

--- Black Jack.py
+++ Black Jack.py
@@ -1,15 +1,13 @@
 import random
 
 cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11] * 4 #Колода карт
-
 random.shuffle(cards) #Перемешиваем колоду
-
 main_pack = 0 #Изначальный баланс игрока
 croupier_pack = 0 #Изначальный баланс крупье
 
 def get_cards_main():
     global main_pack #Обращение к глобальной переменной main_pack (Баланс игрока)
-    print('Выпала карта - ' + str(cards[-1])) #Выводим сообщение о том, какая выпала карта
+    print('\nВыпала карта - ' + str(cards[-1])) #Выводим сообщение о том, какая выпала карта
     main_pack += cards.pop() #Добавляем последнюю карту с колоды к балансу игрока, после удаляем её
     return
 
@@ -20,21 +18,30 @@ def get_cards_croupier():
         croupier_pack += cards.pop() #Добавляем последнюю карту с колоды к балансу бота, после удаляем её
     return 
 
+def set_default(): #Ставим стандарт
+    global cards
+    global main_pack
+    global croupier_pack
+
+    cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11] * 4 #Колода карт
+    random.shuffle(cards) #Перемешиваем колоду
+    main_pack = 0 #Изначальный баланс игрока
+    croupier_pack = 0 #Изначальный баланс крупье
+    return
+
 def send_message(player): #Сообщение о выигрыше, выводим сообщение с балансами обоих игроков
-    print('\n\n\n\n\n\n\n\n\n\n\n') #Пропускаем строки
     if player == 'self':
-        print('Результат: Вы выиграли!')
+        print('\nРезультат: Вы выиграли!')
     elif player == 'croupier':
-        print('Результат: Вы проиграли!')
+        print('\nРезультат: Вы проиграли!')
     elif player == 'none':
-        print('Результат: Ничья')
-    print('Ваш баланс: ' + str(main_pack)) #Баланс игрока
-    print('Баланс крупье: ' + str(croupier_pack) + '\n') #Баланс крупье
+        print('\nРезультат: Ничья')
+    print('\nВаш баланс: ' + str(main_pack) + '\nБаланс крупье: ' + str(croupier_pack) + '\n') #Баланс игрока и крупье
     return
 
 def get_winner(): #Узнаём победителя
-    global croupier_pack #Обращение к глобальной переменной croupier_pack (Баланс крупье)
-    global main_pack #Обращение к глобальной переменной main_pack (Баланс игрока)
+    global croupier_pack #Обращение к глобальной переменной KRUPIE_KOLODA (Баланс крупье)
+    global main_pack #Обращение к глобальной переменной MAIN_KOLODA (Баланс игрока)
     
     if main_pack <= 21 and croupier_pack > 21:
         send_message('self') #Вывод сообщения о победе
@@ -53,9 +60,8 @@ def get_winner(): #Узнаём победителя
 while True:
     try:
         if main_pack < 21:
-            print('Баланс: ', main_pack) #Выводим баланс игрока
+            print('\nБаланс: ', main_pack) #Выводим баланс игрока
             choice = input('Взять карту? [Y/N]: ') #Запрашиваем разрешение о выдаче карты
-            print('\n\n\n\n\n\n\n\n\n\n\n') 
             if choice == 'Y' or choice == 'y' or choice == 'н' or choice == 'Н':
                 
                 #При согласии, отдаем разные карты игроку и крупье
@@ -66,12 +72,21 @@ while True:
             if choice == 'N' or choice == 'n' or choice == 'т' or choice == 'Т': #Отказ от выдачи карты
                 get_cards_croupier() #Крупье берёт карту
                 get_winner() #Выявляем победителя
-                break
+                select = input('\n\nНачать заного? [Y/N]: ')
+                if select == 'Y' or select == 'y' or select == 'н' or select == 'Н':
+                    set_default()
+                    continue
             else:
-                print('[Ошибка] Введите нужный символ') #Ошибка, если не введен нужный символ
+                print('\n\n[Ошибка] Введите нужный символ') #Ошибка, если не введен нужный символ
                 continue
         else:
             get_winner() #Выявляем победителя
-            break
+            select = input('\n\nНачать заного? [Y/N]: ')
+            if select == 'Y' or select == 'y' or select == 'н' or select == 'Н':
+                set_default()
+                continue
+            else:
+                print('\n\nСпасибо за игру!')
+                break
     except ValueError:
-        print('[Ошибка] Введите нужный символ') #Ошибка, если не введен нужный символ, завершаем работу
+        print('\n\n[Ошибка] Введите нужный символ') #Ошибка, если не введен нужный символ, завершаем работу
